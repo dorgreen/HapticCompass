6cff40b7ab39e3e118fa2df2dcaf9c3e3411b0d9
dorgreen <dorgreen@gmail.com>
2017-05-16 23:41:13 +0300

    Added deep sleep
    2 seconds cycle with 0.5 "work time" and 1.5 seconds of sleep.
    * fixed some misc bugs

--- HapticCompass.ino
+++ HapticCompass.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
+#include <LowPower.h>
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
@@ -8,16 +9,21 @@ sensors_event_t event;
 float heading;
 float headingDegrees;
 
+/*
+ * Parameters
+ */
 float declinationAngle = 7.7;
 int actions[] = {8,9,10,11};
-int actionTime = 20;
+int actionTime = 500; // milliseconds
 boolean hardwareTest = false;
 boolean debug = false;
 boolean serial_singleton = false;
 
 void serialSingleton(){
-  if(!serial_singleton)
-    Serial.begin(9600);  
+  if(!serial_singleton){
+    Serial.begin(9600);
+    serial_singleton = true; 
+  } 
 }
 
 void displayReadings(){
@@ -100,9 +106,8 @@ void loop() {
   //Serial.println(headingDegrees);
   act(headingDegrees);
 
-  delay(100);
-  
-  
-  
+  LowPower.idle(SLEEP_1S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF); 
+  LowPower.idle(SLEEP_500MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
+  // "TWI_OFF" turns off Wire\I2C. could mess with magnometer...
 
 }
